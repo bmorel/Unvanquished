bdb09c07c240fa9024ccfdad789342d490139fe7
Morel Bérenger <berengermorel76@gmail.com>
2021-05-06 15:23:26 +0200

    refactoring
    * Create and use BotChangeClass to reduce code duplication
    * Create and use IsWeaponReady to prevent mantis trying to attack while
      in cooldown (less code executed, easier to debug)
    * don't alias a variable 'err' with a less meaninful 'p'

--- src/shared/bg_misc.cpp
+++ src/shared/bg_misc.cpp
@@ -3011,3 +3011,9 @@ namespace VM {
 		playerStateSize = sizeof(playerState_t);
 	}
 }
+
+// checks if a weapon can fire
+bool playerState_t::IsWeaponReady( void ) const
+{
+	return weaponTime <= 0 && ( ammo > 0 || BG_Weapon( weapon )->infiniteAmmo );
+}
